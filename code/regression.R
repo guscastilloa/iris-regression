39d75aeba72a738e1f419a2be486bbb1996bc8d4
Gustavo A. Castillo A <120594773+guscastilloa@users.noreply.github.com>
2026-02-17 19:48:30 -0500

    añadir analisis de regresión

--- code/regression.R
+++ code/regression.R
@@ -1,3 +1,8 @@
 data(iris)
-model <- lm(Sepal.Length ~ Sepal.Width + Petal.Length, data = iris)
+model <- lm(
+  Sepal.Length ~ Sepal.Width + Petal.Length,
+  data = iris)
+model2 <- lm(
+  Sepal.Width ~ Sepal.Length + Petal.Length,
+  data = iris)
 summary(model)
