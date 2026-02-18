8a2402f3a238ef0bcafc902360f325e79dd22f40
Gustavo A. Castillo A <120594773+guscastilloa@users.noreply.github.com>
2026-02-17 20:01:13 -0500

    add additional regression model summaries

--- code/regression.R
+++ code/regression.R
@@ -5,4 +5,9 @@ model <- lm(
 model2 <- lm(
   Sepal.Width ~ Sepal.Length + Petal.Length,
   data = iris)
+model3 <- lm(
+  Petal.Length ~ Sepal.Length + Sepal.Width,
+  data = iris)
 summary(model)
+summary(model2)
+summary(model3)
